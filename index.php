1242024924c36aa7653b2493dd9b9ad5780839b0
Bencece <zahoranbence@hotmail.com>
2020-03-04 20:57:56 +0100

    Move menu to another file

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
 <!DOCTYPE html>
 <html lang="hu">
   <head>
-    <title>Hello Világ!</title>
+    <title>Főoldal</title>
     <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
     <div class="header">
       <h1 id="title">Pepe pizzéria</h1>
-      <nav class="menu">
-        <ul>
-          <li><a href="index.php">Főoldal</a></li>
-          <li><a href="pizza.php">Pizzáink</a></li>
-          <li><a href="cart.php">Kosár</a></li>
-          <li><a href="contact.php">Elérhetőség</a></li>
-        </ul>
-      </nav>
+      <?php include "menu.php"; ?>
     </div>
     <div class="content">asdf</div>
     <div class="footer">avasd</div>
---
+++ pizza.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hu">
+  <head>
+    <title>Pizzáink</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="header">
+      <h1 id="title">Pepe pizzéria</h1>
+      <?php include "menu.php"; ?>
+    </div>
+    <div class="content">asdf</div>
+    <div class="footer">avasd</div>
+  </body>
+</html>
