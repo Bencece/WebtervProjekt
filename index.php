8dde0659774043caf33c0c7e77ae81d5578bd4f4
Bencece <zahoranbence@hotmail.com>
2020-03-04 20:31:32 +0100

    Add peges and do some style changes

--- index.php
+++ index.php
@@ -5,7 +5,16 @@
     <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
-    <div class="header">asdf</div>
+    <div class="header">
+      <nav class="menu">
+        <ul>
+          <li><a href="index.php">Főoldal</a></li>
+          <li><a href="pizza.php">Pizzáink</a></li>
+          <li><a href="cart.php">Kosár</a></li>
+          <li><a href="contact.php">Elérhetőség</a></li>
+        </ul>
+      </nav>
+    </div>
     <div class="content">asdf</div>
     <div class="footer">avasd</div>
   </body>
