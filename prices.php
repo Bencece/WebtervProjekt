6b31eab3a930629af78c88bc477278d4272cdb0f
Bencece <zahoranbence@hotmail.com>
2020-03-05 12:27:31 +0100

    Made a table for prices

--- prices.php
+++ prices.php
@@ -11,7 +11,38 @@
       <?php include "menu.php"; ?>
     </div>
     <div class="content">
-        <p>Áraink<br/>Táblázat beillesztése ide</p>
+        <table>
+          <caption>Áraink</caption>
+          <thead>
+            <tr>
+              <td>Termék</td>
+              <td>Mennyiség</td>
+              <td>Ár</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Pizza1</td>
+              <td>28cm</td>
+              <td>1200</td>
+            </tr>
+            <tr>
+              <td>Pizza2</td>
+              <td>28cm</td>
+              <td>1150</td>
+            </tr>
+            <tr>
+              <td>Pizza3</td>
+              <td>30cm</td>
+              <td>1220</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colspan="3">Az áraink forintban értendőek, és az áfát tartalmazzák.</td>
+            </tr>
+          </tfoot>
+        </table>
     </div>
     <div class="footer">Pizza</div>
   </body>
