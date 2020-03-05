fe67dce01d64ebfe676330901395b78b3161de90
Bencece <zahoranbence@hotmail.com>
2020-03-05 12:02:20 +0100

    Add prices page

--- menu.php
+++ menu.php
@@ -3,6 +3,7 @@
         <li><a href="index.php">Főoldal</a></li>
         <li><a href="pizza.php">Pizzáink</a></li>
         <li><a href="cart.php">Kosár</a></li>
+        <li><a href="prices.php">Áraink</a></li>
         <li><a href="contact.php">Elérhetőség</a></li>
     </ul>
 </nav>
---
+++ prices.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="hu">
+  <head>
+    <title>Áraink</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <?php include "meta.php"; ?>
+  </head>
+  <body>
+    <div class="header">
+      <h1 id="title">Pepe pizzéria</h1>
+      <?php include "menu.php"; ?>
+    </div>
+    <div class="content">
+        <p>Áraink<br/>Táblázat beillesztése ide</p>
+    </div>
+    <div class="footer">Pizza</div>
+  </body>
+</html>
