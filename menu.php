16947aa70f33764b5a87ba91729bd2ee7980d45b
Bencece <zahoranbence@hotmail.com>
2020-03-07 20:37:01 +0100

    Menupontok hattere aktiv, az adott oldalon

--- menu.php
+++ menu.php
@@ -1,10 +1,17 @@
 <nav class="menu">
     <ul>
-        <li><a href="index.php">Főoldal</a></li>
-        <li><a href="pizza.php">Pizzáink</a></li>
-        <li><a href="cart.php">Kosár</a></li>
-        <li><a href="prices.php">Áraink</a></li>
-        <li><a href="contact.php">Elérhetőség</a></li>
+        <li><a href="index.php" <?php print(activateIt("/WebtervProjekt/index.php")); ?>>Főoldal</a></li>
+        <li><a href="pizza.php" <?php print(activateIt("/WebtervProjekt/pizza.php")); ?>>Pizzáink</a></li>
+        <li><a href="cart.php" <?php print(activateIt("/WebtervProjekt/cart.php")); ?>>Kosár</a></li>
+        <li><a href="prices.php" <?php print(activateIt("/WebtervProjekt/prices.php")); ?>>Áraink</a></li>
+        <li><a href="contact.php" <?php print(activateIt("/WebtervProjekt/contact.php")); ?>>Elérhetőség</a></li>
         <!-- <li><a href="login.php">Bejelentkezés</a></li> -->
     </ul>
 </nav>
+<?php
+    function activateIt($oldal){
+        if($_SERVER['SCRIPT_NAME'] == $oldal){
+            return "style='background-color: darkgray';";
+        }
+    }
+?>
